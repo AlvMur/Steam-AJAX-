79859c68ae2afb53107399711dc56f1a10dd4f53
xdaraboz <73521177+xdaraboz@users.noreply.github.com>
2021-02-23 13:26:37 +0100

    biblioteca $getJSON

--- JS/codigo.js
+++ JS/codigo.js
@@ -95,6 +95,8 @@ function ocultarFormularios() {
     if (document.querySelector("#listadoJuegos") != null)
     //    document.querySelector("#listadoJuegos").remove();
         document.querySelector("#listadoJuegos").style.display = "none";
+    if(document.querySelector("#listadoJuegosCliente")!=null)
+    document.querySelector("#listadoJuegosCliente").style.display="none";
     // if (oTabla != null) {
     //     for (let index = 0; index < oTabla.length; index++) {
     //         oTabla[index].remove();
@@ -231,6 +233,7 @@ function altaJuego() {
         xmlhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
         alert("Juego insertado");
+        //console.log(xmlhttp);
         //alert(xmlhttp.responseText);
         }
     };
@@ -359,6 +362,7 @@ function bibliotecaBuscada() {
                 dataType: 'json',
                 url: 'PHP/recogerbiblioteca.php',
                 data: {email:sEmail},
+                type:'POST',
                 success: function (data) {
                   // begin accessing JSON data here
                   console.log(JSON.stringify(data));
@@ -377,8 +381,19 @@ function respuestaBiblioteca(data,status,oXHR) {
     console.log(status);
     console.log(oXHR);
    
+    let divBiblioteca = document.querySelector("#listadoJuegosCliente");
     
+    let tabla = divBiblioteca.querySelector("#lista");
+    let tBody = tabla.querySelector("tbody");
+    tBody.innerHTML="";
 
+    for (let index = 0; index < data.length; index++) {
+        
+        tBody.innerHTML+="<tr><td>"+data[index].titulo+"</td><td>"+data[index].genero+"</td><td>"+data[index].año_lanzamiento+"</td><td>"+data[index].precio+"</td><td>"+data[index].pegi+"</td></tr>";
+        
+    }
+
+    divBiblioteca.style.display="block";
 
     
 }
